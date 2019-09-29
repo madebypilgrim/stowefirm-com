c220307f6970905fbda8a6691b2203b0ddbe3862
Trevor Plassman <tplassman@wrayward.com>
2019-09-28 22:47:51 -0400

    initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,45 @@
+import pop from 'compop';
+import { spy } from 'ui-utilities';
+import './polyfills';
+
+// Components
+// import Header from './components/header';
+
+const { SITE_HANDLE } = process.env;
+
+// Define map of component handles to component classes
+/* eslint-disable quote-props */
+const classMap = {
+    // 'header': Header,
+};
+/* eslint-enable quote-props */
+
+// Define all actions/commands that components pub/sub
+const actions = {
+    // Action events
+    lockScroll: 'lock-scroll',
+    unlockScroll: 'unlock-scroll',
+    showHeader: 'show-header',
+    hideHeader: 'hide-header',
+    openModal: 'open-modal',
+    closeModal: 'close-modal',
+    loadModal: 'load-modal',
+    showInputErrors: 'show-input-errors',
+};
+
+// Event handler functions
+function handleDOMConentLoaded() {
+    const scaffold = window[SITE_HANDLE];
+
+    // Functionality for after components initialize
+    function cb() {
+        // Check lazy loaded images
+        spy.images();
+    }
+
+    // Call component constructors
+    pop({ scaffold, classMap, actions, cb });
+}
+
+// Add event listeners
+document.addEventListener('DOMContentLoaded', handleDOMConentLoaded);
