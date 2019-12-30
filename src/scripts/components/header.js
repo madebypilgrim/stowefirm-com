b256697696e182ce5d457559fb636b036b63db3b
Trevor Plassman <tplassman@wrayward.com>
2019-12-29 21:42:40 -0500

    beter homepage hero

--- src/scripts/components/header.js
+++ src/scripts/components/header.js
@@ -4,7 +4,5 @@ export default class {
     }) {
         const el = document.getElementById(id);
         const toggle = el.querySelector('[name="nav-toggle"]');
-
-        toggle.checked = true;
     }
 }
