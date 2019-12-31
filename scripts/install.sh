c49bac30185e3286b01895e5d94f2a32d41ef510
Trevor Plassman <tplassman@wrayward.com>
2019-12-30 21:02:05 -0500

    hover styles

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,47 +1,6 @@
 #!/bin/bash
 
-# TODO:
 # The purpose of this script is to install all dependencies for Craft CMS.
-#
-# This script requires a sibling file named ".env" that contains named
-# variabels as follows:
-#
-# |_ DB_DATABASE (craft CMS variable)
-# |_ DB_USER (craft CMS variable)
-# |_ DB_PASSWORD (craft CMS variable)
-# |_ SSH_HOST
-# |_ STAG_DB_DATABASE
-# |_ STAG_DB_USER
-# |_ STAG_DB_PASSWORD
-# |_ STAG_HOME_PATH
-# |_ PROD_DB_DATABASE
-# |_ PROD_DB_USER
-# |_ PROD_DB_PASSWORD
-# |_ PROD_HOME_PATH
-
-# Import env variables
-source .env
-
-# Constants
-
-# Initialize variables
-# REMOTE_DB_DATABASE=""
-# REMOTE_DB_USER=""
-# REMOTE_DB_PASSWORD=""
-# REMOTE_HOME_PATH=""
-
-# Set remote environment
-# if [ "$1" = "staging" ]; then
-#   REMOTE_DB_DATABASE="$STAG_DB_DATABASE"
-#   REMOTE_DB_USER="$STAG_DB_USER"
-#   REMOTE_DB_PASSWORD="$STAG_DB_PASSWORD"
-#   REMOTE_HOME_PATH="$STAG_HOME_PATH"
-# elif [ "$1" = "production" ]; then
-#   REMOTE_DB_DATABASE="$PROD_DB_DATABASE"
-#   REMOTE_DB_USER="$PROD_DB_USER"
-#   REMOTE_DB_PASSWORD="$PROD_DB_PASSWORD"
-#   REMOTE_HOME_PATH="$PROD_HOME_PATH"
-# fi
 
 ################################################################################
 # Create DB and USER w/ account
